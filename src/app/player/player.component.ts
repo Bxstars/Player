d6a412f32cd5bfc2d3f42e2c4093ae59d7adfed5
Bxstars <bxstars4@gmail.com>
2023-03-19 19:36:48 -0400

    [bug-fix] Ao ativar o modo dark ; Alterada as cores para o modo dark

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -3,51 +3,55 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.css']
+  styleUrls: ['./player.component.css'],
 })
 export class PlayerComponent implements AfterViewInit {
-  myVideo:any;
+  myVideo: any;
   status: boolean = true;
+  modeDark: boolean = false;
 
-  constructor () {}
+  constructor() { }
 
   ngAfterViewInit(): void {
-  this.myVideo = document.getElementById("catHarry") as HTMLVideoElement | null;
+    this.myVideo = document.getElementById(
+      'catHarry'
+    ) as HTMLVideoElement | null;
   }
 
-  playPause() {
-    if (this.myVideo != null) {
-      this.myVideo.paused ? this.myVideo.play() : this.myVideo?.pause();
-      this.status = this.myVideo.paused
-    }
-  }
-
-  makeFull() { // 720p (HD): 1280 x 720
+  /* #region screen controls */
+  makeBig() {
+    // 720p (HD): 1280 x 720
     this.myVideo.width = 1280;
     this.myVideo.height = 720;
   }
 
-  makeNormal() { //360p (SD): 640 x 360
+  makeNormal() {
+    //360p (SD): 640 x 360
     this.myVideo.width = 640;
     this.myVideo.height = 360;
   }
 
-  makeSmall() { //240p (SD): 426 x 240
+  makeSmall() {
+    //240p (SD): 426 x 240
     this.myVideo.width = 426;
     this.myVideo.height = 240;
   }
+  /* #endregion */
 
-  forward() {
-    console.log('passou aqui')
-    this.myVideo as HTMLVideoElement
-    this.myVideo.playbackRate = 5.0;
-    // let video = document.getElementById("catHarry") as HTMLVideoElement;
-    // video.defaultPlaybackRate = 2.0;
-
+  playPause() {
+    if (this.myVideo != null) {
+      this.myVideo.paused ? this.myVideo.play() : this.myVideo?.pause();
+      this.status = this.myVideo.paused;
+    }
   }
 
-  modeScreen(event:any) {
-   const html = document.querySelector('html') as HTMLElement
-   html.classList.toggle('dark-mode')
+  forward() {
+    this.myVideo as HTMLVideoElement;
+    this.myVideo.playbackRate = 2.0;
+  }
+
+  darkOrLight(event: any) {
+    const html = document.querySelector('html') as HTMLElement;
+    html.classList.toggle('dark-mode');
   }
 }
