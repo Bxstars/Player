e3e0b917ed87becebbe22d5c73c9df2ee903d616
Barbara Xavier <bxstars4@gmail.com>
2026-02-17 20:07:21 -0400

    fix: corrects the expansion and dimensions of the video.

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -11,6 +11,7 @@ export class PlayerComponent implements AfterViewInit {
   status: boolean = true;
   modeDark: boolean = false;
   videoEnd: boolean = true;
+  isBig: boolean = false;
 
   constructor() {}
 
@@ -31,19 +32,10 @@ export class PlayerComponent implements AfterViewInit {
     this.videoEnd = this.myVideo.ended;
   }
 
-  /* #region screen controls */
-  makeSmall() {
-    //240p (SD): 426 x 240
-    this.myVideo.width = 426;
-    this.myVideo.height = 240;
-  }
 
-  makeBig() {
-    // 720p (HD): 1280 x 720
-    this.myVideo.width = 1280;
-    this.myVideo.height = 400;
+  toggleSize() {
+    this.isBig = !this.isBig;
   }
-  /* #endregion */
 
   playPause() {
     this.getDuration();
